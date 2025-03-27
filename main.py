5709de958792f9f6fc966c51fcda1557e46a5579
Curtis <curtis.harrison02@gmail.com>
2025-03-27 15:16:39 -0600

    move tirangle

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -13,13 +19,18 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
+        updatable.update(dt)
+
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         
-        dt += clock.tick(60)/1000
+        dt = clock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
