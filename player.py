8222fee5de4ce3caee38a4108b0bb033cb966d81
Curtis <curtis.harrison02@gmail.com>
2025-03-28 13:55:35 -0600

    shots enabled

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from circleshape import *
 from constants import *
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -33,6 +34,13 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_w]:
             self.move(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def shoot(self):
+        new_shot = Shot(self.position.x, self.position.y)
+        new_shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
